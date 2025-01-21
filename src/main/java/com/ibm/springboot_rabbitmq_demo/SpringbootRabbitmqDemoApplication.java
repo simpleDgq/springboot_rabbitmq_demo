c18762febc9415c4d1f356693c13e32db075f51c
GuoQingDeng <1255826318@qq.com>
2025-01-21 11:22:44 +0800

    initial commit

---
+++ src/main/java/com/ibm/springboot_rabbitmq_demo/SpringbootRabbitmqDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.springboot_rabbitmq_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRabbitmqDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRabbitmqDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ibm/springboot_rabbitmq_demo/SpringbootRabbitmqDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.springboot_rabbitmq_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRabbitmqDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
